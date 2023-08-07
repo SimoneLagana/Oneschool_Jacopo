80590b23c9c42260312fe25b0f15bf553f8bfff4
SimoneLagana <lagana.1946083@studenti.uniroma1.it>
2023-08-07 20:47:39 +0200

    Installato bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root "articles#index"
+
+  get "/articles", to: "articles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
