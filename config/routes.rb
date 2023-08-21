4ea7bb62551ad90301b97fdf2b00d1e2fb69025d
SimoneLagana <lagana.1946083@studenti.uniroma1.it>
2023-08-21 17:25:07 +0200

    Mi piace compilare i form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'meeting/index'
   root "articles#index"
 
   get "/articles", to: "articles#index"
+
+  get "/meeting", to: "meeting#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- test/models/school_staff_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class SchoolStaffTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
